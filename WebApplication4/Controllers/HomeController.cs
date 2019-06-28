b9d13d81fcb80d31f6e55e96f105044968e2616d
张飞 <494126372@qq.com>
2019-06-28 21:36:33 +0800

    ModelState.IsValid 会主动调用验证 只需在WebAPIConfig中配置单独交验就需要加过滤器将ModelState.IsValid 校验处理写在ValidateModelStateFilter中然后Action处做注解就行了

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication4.Models.Product;
@@ -25,13 +27,18 @@ namespace WebApplication4.Controllers
         {
             //CustomerValidator validator = new CustomerValidator();
             //ValidationResult results = validator.Validate(customer);
-            if (!ModelState.IsValid)
-            {
+            //if (!ModelState.IsValid) ///此句话很重要 加上会不用过滤器直接走校验规则坏处是每个Action都要写一遍 所以添加全局验证Filter拦截中写上统一的
+            //{
+            //    var errors = ModelState.Values.Select(x => x.Errors).First().ToString();
+            //    ViewBag.err = errors == "" ? "无错误" : errors;
+
+            //    var response = new HttpResponseMessage();
+            //    response.Content = new StringContent(string.Join("\n", errors));
+            //    response.StatusCode = HttpStatusCode.BadRequest;
+            //    throw new System.Web.Http.HttpResponseException(response);
                
-               var errors = ModelState.Values.Select(x => x.Errors).First().ToString();
-                ViewBag.err = errors == "" ? "无错误" : errors ;
                   
-            }
+            //}
             return View(product);
         }
         [HttpGet]  // 不加过滤器的用法 
--- WebApplication4/Controllers/ProductController.cs
+++ WebApplication4/Controllers/ProductController.cs
@@ -12,9 +12,10 @@ namespace WebApplication4.Controllers
     {
         // 不加过滤器的用法  不需要再全局Global中注册 需要在WebAPIConfig中加并添加注解 对所有方法接口奏效
         //MVC中不能这么用 必须全局注册下 并加注解    FluentValidationModelValidatorProvider.Configure();
+        // 此接口也可以调用 验证因为全局注册了 !ModelState.IsValid 会主动调用
         public IHttpActionResult Post([FromBody]CustomProduct model)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid) // 主动走RuleFor规则校验 这句话主动调用！！！！！
             {
                 return BadRequest(ModelState);
             }
